551c60553558ef876f25371435472964300ef7b5
fpr <frankpiercereynolds95@gmail.com>
2022-09-19 21:41:59 -0400

    updated character model again

--- models/Character.js
+++ models/Character.js
@@ -19,7 +19,12 @@ Character.init({
       key: "id",
     },
   },
-  date_created: {}
+  //   date....... created
+  date_created: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: dataTypes.NOW,
+  },
   //   character name
   name: {
     type: DataTypes.STRING,
@@ -33,5 +38,186 @@ Character.init({
   //   character level
   level: {
     type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  background: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  race: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  alignment: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  experience_points: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  strength: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  dexterity: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  constitution: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  intelligence: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  wisdom: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  charisma: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  //   saves
+  strength_save: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  dexterity_save: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  constitution_save: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  intelligence_save: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  wisdom_save: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  charisma_save: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  //   skills
+  acrobatics: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  animal_handling: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  arcana: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  athletics: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  deception: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  history: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  insight: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  intimidation: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  investigation: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  medicine: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  nature: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  perception: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  performance: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  persuasion: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  religion: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  sleight_of_hand: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  stealth: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  survival: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  // misc character values (hp, ac, prof, insp, init, speed)
+  inspiration: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  proficiency_bonus: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  ac: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  initiative: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  speed: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  hp_max: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  hp_current: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  hit_dice: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  //   death saves
+  ds_success: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  ds_failure: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
   },
 });
