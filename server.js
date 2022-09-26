57a186798cab82839ab758a2b4df8c96cb7b5576
fpr <frankpiercereynolds95@gmail.com>
2022-09-26 17:23:00 -0400

    updated pathing to import style/js

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(__dirname + "/public"));
 
 app.use(routes);
 
