4761ee401921e9807c00f65a23cb6b82e0cd6977
aalissy <32420245+aalissy@users.noreply.github.com>
2022-09-26 19:14:40 -0500

    Updating controllers ty Tim!

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -1,111 +1,203 @@
-const router = require("express").Router();
-const { Character } = require("../../models");
+const router = require('express').Router();
+const { User, Character } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get("/mycharacters", (req, res) => {
-  Character.findAll({
-    attributes: ["user_id", "name"],
-  }).then((dbplayerData) => {
-    res.json(dbplayerData);
-  });
+// Getting all characters
+router.get('/', (req, res) => {
+    Character.findAll({
+        attributes: [
+            'id',
+            'strength',
+            'dexterity',
+            'constitution',
+            'intelligence',
+            'wisdom',
+            'charisma',
+            'strength_save',
+            'dexterity_save',
+            'cosntitution_save',
+            'intelligence_save',
+            'wisdom_save',
+            'charisma_save',
+            'proficiency_bonus',
+            'acrobatics',
+            'animal_handling',
+            'arcana',
+            'athletics',
+            'deception',
+            'history',
+            'insight',
+            'intimidation',
+            'investigation',
+            'medicine',
+            'nature',
+            'perception',
+            'performance',
+            'persuasion',
+            'religion',
+            'sleight_of_hand',
+            'stealth',
+            'survival'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+        .then(data => res.json(data))
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const dbPlayerData = await Character.findAll({
-      include: [
-        {
-          attributes: ["name", "level"],
+// Getting characters based on id
+router.get('/:id', (req, res) => {
+    Character.findOne({
+        where: {
+            id: req.params.id
         },
-      ],
-    });
-    dbPlayerData.map((char) => char.get({ plain: true }));
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+        attributes: [
+          'id',
+          'strength',
+          'dexterity',
+          'constitution',
+          'intelligence',
+          'wisdom',
+          'charisma',
+          'strength_save',
+          'dexterity_save',
+          'cosntitution_save',
+          'intelligence_save',
+          'wisdom_save',
+          'charisma_save',
+          'proficiency_bonus',
+          'acrobatics',
+          'animal_handling',
+          'arcana',
+          'athletics',
+          'deception',
+          'history',
+          'insight',
+          'intimidation',
+          'investigation',
+          'medicine',
+          'nature',
+          'perception',
+          'performance',
+          'persuasion',
+          'religion',
+          'sleight_of_hand',
+          'stealth',
+          'survival'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({
+                    message: "I'm sorry that character wasn't found! Please try again later."
+                });
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-router.get("/mycharacters/:id", async (req, res) => {
-  Character.findOne({
-    where: {
-      id: req.params.id,
+// Creating new characters
+router.post('/', withAuth, (req, res) => {
+    Character.create({
+        user_id: req.session.user_id,
+        strength: req.body.strength,
+        dexterity: req.body.dexterity,
+        constitution: req.body.constitution,
+        intelligence: req.body.intelligence,
+        wisdom: req.body.wisdom,
+        charisma: req.body.charisma,
+        strength_save: req.body.strength_save,
+        dexterity_save: req.body.dexterity_save,
+        constitution_save: req.body.constitution_save,
+        intelligence_save: req.body.intelligence_save,
+        wisdom_save: req.body.wisdom_save,
+        charisma_save: req.body.charisma_save,
+        level: req.body.level,
+        proficiency_bonus: req.body.proficiency_bonus,
+        acrobatics: req.body.acrobatics,
+        animal_handling: req.body.animal_handling,
+        arcana: req.body.arcana,
+        athletics: req.body.athletics,
+        deception: req.body.deception,
+        history: req.body.history,
+        insight: req.body.insight,
+        intimidation: req.body.intimidation,
+        investigation: req.body.investigation,
+        medicine: req.body.medicines,
+        nature: req.body.nature,
+        perception: req.body.perception,
+        performance: req.body.performance,
+        persuasion: req.body.persuasion,
+        religion: req.body.religion,
+        sleight_of_hand: req.body.sleight_of_hand,
+        stealth: req.body.stealth,
+        survival: req.body.survival
+    })
+        .then(data => res.json(data))
+        .catch(err => {
+            res.status(500).json(err)
+        });
+});
+
+// Updating characters
+router.put('/:id', withAuth, (req, res) => {
+    Character.update({
+        level: req.body.level
     },
-    attributes: ["id", "name", "level", "class", "race", "background"],
-  })
-    .then((data) => {
-      if (!data) {
-        res.status(404).json({ message: "No player found with this id" });
-        return;
-      }
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({
+                    message: "I'm sorry that character wasn't found! Please try again later!"
+                });
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-router.post("/", (req, res) => {
-  Character.create({
-    level: req.body.level, 
-    strength: req.body.strength, 
-    dexterity: req.body.dexterity, 
-    constitution: req.body.constitution, 
-    intelligence: req.body.intelligence, 
-    wisdom: req.body.wisdom, 
-    charisma: req.body.charisma, 
-    strength_save: req.body.strength_save, 
-    dexterity_save: req.body.dexterity_save, 
-    consitution_save: req.body.consitution_save, 
-    intelligence_save: req.body.intelligence_save, 
-    wisdom_save: req.body.wisdom_save, 
-    charisma_save: req.body.charisma_save,
-    race: req.body.race, 
-    proficiency_bonus: req.body.proficiency_bonus, 
-    acrobatics: req.body.acrobatics, 
-    animal_handling: req.body.animal_handling, 
-    arcana: req.body.arcana, 
-    athletics: req.body.athletics, 
-    deception: req.body.deception, 
-    history: req.body.history, 
-    insight: req.body.insight, 
-    intimidation: req.body.intimidation, 
-    investigation: req.body.investigation, 
-    medicine: req.body.medicines, 
-    nature: req.body.nature, 
-    perception: req.body.perception, 
-    performance: req.body.performance, 
-    persuasion: req.body.persuasion, 
-    religion: req.body.religion, 
-    sleight_of_hand: req.body.sleight_of_hand, 
-    stealth: req.body.stealth, 
-    survival: req.body.survival
-  })
-    .then((data) => {
-      res.json(data);
+// Deleting characters
+router.delete('/:id', withAuth, async (req, res) => {
+    Character.destroy({
+        where: {
+            id: req.params.id
+        }
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.delete("/mycharacters/:id", (req, res) => {
-  Character.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((data) => {
-      if (!data) {
-        res.status(404).json({ message: "No player found with this id" });
-        return;
-      }
-      res.json(data);
+    .then(data => {
+        if (!data) {
+            res.status(404).json({
+                message: "I'm sorry that character wasn't found! Please try again later!"
+            });
+            return;
+        }
+        res.json(data);
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+    .catch(err => {
+        res.status(500).json(err);
     });
 });
 
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,153 +1,153 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Character } = require('../../models/index');
+const withAuth = require('../../utils/auth');
 
-//Get all users
+// Getting all users 
 router.get('/', (req, res) => {
-  User.findAll({
-    attributes: {exclude: ['password']}
-  })
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-}); 
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(data => res.json(data))
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
-// Get a single user 
+// Getting users based on id
 router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: {exclude: ['password']},
-  })
-// get all users
-
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-})
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Character,
+                attributes: [
+                    'id', 'user_id', 'level'
+                ]
+            }
+        ]
+    })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({
+                    message: "I'm sorry that user wasn't found! Please try again later!"
+                });
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-// CREATE new user
-router.post('/', async (req, res) => {
-  try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      res.status(200).json(dbUserData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+// Creating new users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(data => {
+            req.session.user_id = data.id;
+            req.session.username = data.username;
+            req.session.loggedIn = true;
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-// Update existing user
-// update user 
-router.put('/:id', (req, res) => {
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbUserData => {
-    if(!dbUserData[0]) {
-      res.status(404).json({ message: 'No user found with this id' });
-      return;
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+// Editing user data by id
+router.put('/:id', withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(data => {
+            if (!data) {
+                res.status(400).json({
+                    message: "I'm sorry that user wasn't found! Please try again later!"
+                });
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
-// delete user
-router.delete('/:id', (req, res) => {
-  User.destroy({
-    where: {
-      id: req.params.id,
-    }
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+// Deleting user by id
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({
+                    message: "I'm sorry that user wasn't found! Please try again later!"
+                });
+                return;
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+// Posting the login page
+router.post('/login', async (req, res) => {
+    try {
+      const data = await User.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+      if (!data) {
+        console.log("Couldn't find user");
+        res
+          .status(400)
+          .json({ message: "Incorrect username or password! Please try again later!" });
         return;
       }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
+      const validPw = await data.checkPassword(req.body.password);
+      if (!validPw) {
+        console.log("Invalid password!");
+        res
+          .status(400)
+          .json({ message: "Incorrect username or password! Please try again later!" });
+        return;
+      }
+      req.session.save(() => {
+        console.log("Logging in", data);
+        req.session.user_id = data.id;
+        req.session.username = data.username;
+        req.session.loggedIn = true;
+        res.json({ user: data, message: "Congratulations! You have successfully logged in!" });
+    });
+    } catch (err) {
       res.status(500).json(err);
-    });
-});
-
-// Login
-router.post('/login', async (req, res) => {
-  try {
-    const dbUserData = await User.findOne({
-      where: {
-        username: req.body.username,
-      },
-    });
-
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password. Please try again!' });
-      return;
     }
+  });
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password. Please try again!' });
-      return;
+// Posting the logout page
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
     }
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      res
-        .status(200)
-        .json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
-});
-
-// Logout
-router.post('/logout', async (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
 });
 
 module.exports = router;
